2fb19a965c7fb5f9eb8da9dbaad31190a5e89ac4
PRANEETH_K04 <praneethkamatham04@gmail.com>
2024-09-08 17:33:21 +0530

    update

---
+++ src/components/first_page/First_Page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function First_Page() {
+  return (
+    <div className='header'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>sign-in</li>
+            <li>sign-up</li>
+        </ul>
+    </div>
+  )
+}
+
+export default First_Page
